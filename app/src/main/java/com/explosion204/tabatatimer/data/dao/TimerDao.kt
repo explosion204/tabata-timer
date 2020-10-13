c74bbe01755e9c8e0944f50ecbfa8179c6d80a16
explosion204 <dzmitriy20magic@gmail.com>
2020-10-13 13:33:56 +0300

    timerdao sql query order fix

--- app/src/main/java/com/explosion204/tabatatimer/data/dao/TimerDao.kt
+++ app/src/main/java/com/explosion204/tabatatimer/data/dao/TimerDao.kt
@@ -6,7 +6,7 @@ import com.explosion204.tabatatimer.data.entities.Timer
 
 @Dao
 interface TimerDao : BaseDao<Timer> {
-    @Query("SELECT * FROM timer_table ORDER BY title DESC")
+    @Query("SELECT * FROM timer_table ORDER BY title ASC")
     fun getAll(): LiveData<List<Timer>>
 
     @Query("SELECT * FROM timer_table WHERE timerId=:id")
