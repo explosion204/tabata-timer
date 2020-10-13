c30a7e0e8851cdf1d1e1b0db2c9eb0014300a213
explosion204 <dzmitriy20magic@gmail.com>
2020-10-13 15:35:53 +0300

    basedao & baserepository: generic constraint for baseentity

--- app/src/main/java/com/explosion204/tabatatimer/data/dao/BaseDao.kt
+++ app/src/main/java/com/explosion204/tabatatimer/data/dao/BaseDao.kt
@@ -1,9 +1,10 @@
 package com.explosion204.tabatatimer.data.dao
 
 import androidx.room.*
+import com.explosion204.tabatatimer.data.entities.BaseEntity
 
 @Dao
-interface BaseDao<T> {
+interface BaseDao<T : BaseEntity> {
     @Update
     fun update(item: T)
 
