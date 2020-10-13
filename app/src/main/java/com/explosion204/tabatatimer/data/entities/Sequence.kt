bec397d2e07a6865ea118f81ac89655cccb64e4f
explosion204 <dzmitriy20magic@gmail.com>
2020-10-13 13:35:26 +0300

    column name for sequence title

--- app/src/main/java/com/explosion204/tabatatimer/data/entities/Sequence.kt
+++ app/src/main/java/com/explosion204/tabatatimer/data/entities/Sequence.kt
@@ -1,10 +1,13 @@
 package com.explosion204.tabatatimer.data.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "seq_table")
 data class Sequence(
-    @PrimaryKey val seqId: Int,
+    @PrimaryKey
+    val seqId: Int,
+    @ColumnInfo(name = "title")
     val title: String
 )
